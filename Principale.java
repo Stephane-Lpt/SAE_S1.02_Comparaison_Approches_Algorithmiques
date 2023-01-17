19c0c158038806751d748eb4e14de0df4c39f3fb
GregoireHirtz <hirtz.gregoire@ghmail.fr>
2023-01-17 12:09:17 +0100

    mise a jour class Principale pour question 7,8,9

--- Principale.java
+++ Principale.java
@@ -2,12 +2,10 @@
  * Classe principale de la SAÉ
  * @author Étienne André Sergueï Lenglet
  * @since 2021-11-04
- *
  */
 
 
 public class Principale{
-
     private static final String[] ELEMENTS_DE_DEBUT
 	= {"ABITEBOUL", "ADLEMAN", "AL-KINDI", "ALUR", "BERNERS-LEE",
 	"BOOLE", "BUCHI", "BUTLER", "CLARKE", "CURRY"};
@@ -39,21 +37,70 @@ public class Principale{
 	
     // Exemple d'utilisation de LectureFichier et remplissage d'une liste
     public static void remplir_liste(ListeTriee liste, String nom_fichier){
-	LectureFichier lf = new LectureFichier(nom_fichier);
-	// 		
-	String[] liste_noms = lf.lireFichier();
-	for (int i = 0; i < liste_noms.length; i++) {
-	    liste.adjlisT(liste_noms[i]);
-	}
+	    LectureFichier lf = new LectureFichier(nom_fichier);	
+        String[] liste_noms = lf.lireFichier();
+        for (int i = 0; i < liste_noms.length; i++){
+            liste.adjlisT(liste_noms[i]);
+        }
     }
 		
     public static void main(String [] args){
-	System.out.println("Bienvenue !");
+        System.out.println("Bienvenue !\n");
 
-	//Exemple d'utilisation de la classe EcritureFichier
-	EcritureFichier fichier = new EcritureFichier("resultats.csv");
-	fichier.ouvrirFichier();
-	fichier.ecrireLigne("liste;operation;emplacement;duree");
-	fichier.fermerFichier();
+        //Exemple d'utilisation de la classe EcritureFichier
+        EcritureFichier fichier = new EcritureFichier("resultats.csv");
+        fichier.ouvrirFichier();
+        fichier.ecrireLigne("liste;operation;emplacement;duree");
+        fichier.fermerFichier();
+
+        ListeTriee lT = new ListeTriee(new ListeChaineePlacesLibres(10010));
+        remplir_liste(lT, "noms10000.txt");
+
+        long temps;
+
+
+        // Question 7
+        System.out.println("Question 7\nadjlisT avec element debut alphabet\n");
+        String[] donneeD={"ABBADI", "ABERGEL", "ALIAS", "ALIOUI", "AKKUS", "ALAZARD","ALLA", "AIDARA", "ABRANTES", "AARAB"};
+        
+        lT = new ListeTriee(new ListeContigue(100000));
+        System.out.println("contigue :"+chronoAdjlis(lT, donneeD));
+        lT = new ListeTriee(new ListeChainee(100000));
+        System.out.println("chaine :"+chronoAdjlis(lT, donneeD));
+        lT = new ListeTriee(new ListeChaineePlacesLibres(100000));
+        System.out.println("chainePlaceLibre :"+chronoAdjlis(lT, donneeD));
+
+        // Question 8
+        System.out.println("\n\nQuestion 8\nadjlisT avec element fin alphabet\n");
+        String[] donneeF={"WEIS", "ZANIN", "WERQUIN", "YAGOUBI", "WERNERT","WAWRZYNIAK", "ZULIANI", "ZAIRE", "WAVRANT", "VILLAR"};
+        
+        lT = new ListeTriee(new ListeContigue(100000));
+        System.out.println("contigue :"+chronoAdjlis(lT, donneeF));
+        lT = new ListeTriee(new ListeChainee(100000));
+        System.out.println("chaine :"+chronoAdjlis(lT, donneeF));
+        lT = new ListeTriee(new ListeChaineePlacesLibres(100000));
+        System.out.println("chainePlaceLibre :"+chronoAdjlis(lT, donneeF))
+
+        // Question 9
+        /**
+         * contigue :
+         * 
+         * chainee : 
+         * 
+         * chainePlaceLibre : parcours toute la liste
+         */
     }
+
+    public static long chronoAdjlis(ListeTriee lT, String[] donnee){
+        remplir_liste(lT, "noms10000.txt");
+        long date_debut = System.nanoTime();
+        for(int i=0; i<donnee.length;i++){
+            lT.adjlisT(donnee[i]);
+        }
+        long date_fin = System.nanoTime();
+        return date_fin - date_debut;
+
+    }
+
+
 }
